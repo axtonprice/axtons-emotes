2055ec8de3557e01aa14566ad39e2619ae4a106b
Axton <axton@arizon.dev>
2024-02-21 15:39:06 +0000

    v1.0 Release

--- src/main/java/arizonsoftware/axtonsemotes/AxtonsEmotes.java
+++ src/main/java/arizonsoftware/axtonsemotes/AxtonsEmotes.java
@@ -7,6 +7,7 @@ import org.bukkit.Sound;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.PluginCommand;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -24,43 +25,47 @@ public class AxtonsEmotes extends JavaPlugin {
                     new String[] { "hug", "Hug another player" }, // command name, description
                     new String[] { "cuddle" }, // aliases
                     new Object[] { "hugged", "c", false }, // past tense verb, colour, self-executable?
-                    new Object[] { Particle.HEART, Sound.ENTITY_VILLAGER_CELEBRATE } // particle and sound
+                    new Object[] { Particle.HEART, true, Sound.ENTITY_VILLAGER_CELEBRATE, true } // particle and sound
             ),
             new CommandItem(
                     new String[] { "kiss", "Kiss another player" }, // command name, description
                     new String[] { "smooch" }, // aliases
                     new Object[] { "kissed", "c", false }, // past tense verb, colour, self-executable?
-                    new Object[] { Particle.VILLAGER_ANGRY, Sound.ENTITY_VILLAGER_HURT } // particle and sound
+                    new Object[] { Particle.VILLAGER_ANGRY, true, Sound.ENTITY_VILLAGER_HURT, true } // particle and
+                                                                                                     // sound
             ),
             new CommandItem(
                     new String[] { "poke", "Poke another player" }, // command name, description
                     new String[] { "prod" }, // aliases
                     new Object[] { "poked", "e", false }, // past tense verb, colour, self-executable?
-                    new Object[] { Particle.VILLAGER_ANGRY, Sound.ENTITY_CHICKEN_EGG } // particle and sound
+                    new Object[] { Particle.VILLAGER_ANGRY, true, Sound.ENTITY_CHICKEN_EGG, true } // particle and sound
             ),
             new CommandItem(
                     new String[] { "highfive", "High-five another player" }, // command name, description
                     new String[] { "hf", "brofist" }, // aliases
                     new Object[] { "high-fived", "b", false }, // past tense verb, colour, self-executable?
-                    new Object[] { Particle.VILLAGER_HAPPY, Sound.ENTITY_VILLAGER_CELEBRATE } // particle and sound
+                    new Object[] { Particle.VILLAGER_HAPPY, true, Sound.ENTITY_VILLAGER_CELEBRATE, true } // particle
+                                                                                                          // and sound
             ),
             new CommandItem(
                     new String[] { "slap", "Slap another player" }, // command name, description
                     new String[] { "hit" }, // aliases
                     new Object[] { "slapped", "c", false }, // past tense verb, colour, self-executable?
-                    new Object[] { Particle.VILLAGER_ANGRY, Sound.ENTITY_VILLAGER_HURT } // particle and sound
+                    new Object[] { Particle.VILLAGER_ANGRY, true, Sound.ENTITY_VILLAGER_HURT, true } // particle and
+                                                                                                     // sound
             ),
             new CommandItem(
                     new String[] { "thank", "Thank another player" }, // command name, description
                     new String[] { "appreciate" }, // aliases
                     new Object[] { "thanked", "b", true }, // past tense verb, colour, self-executable?
-                    new Object[] { Particle.VILLAGER_HAPPY, Sound.ENTITY_VILLAGER_CELEBRATE } // particle and sound
+                    new Object[] { Particle.VILLAGER_HAPPY, true, Sound.ENTITY_VILLAGER_CELEBRATE, true } // particle
+                                                                                                          // and sound
             ),
             new CommandItem(
                     new String[] { "lick", "Lick another player" }, // command name, description
                     new String[] {}, // aliases
                     new Object[] { "licked", "e", false }, // past tense verb, colour, self-executable?
-                    new Object[] { Particle.GLOW_SQUID_INK, Sound.ENTITY_FROG_TONGUE } // particle and sound
+                    new Object[] { Particle.GLOW_SQUID_INK, true, Sound.ENTITY_FROG_TONGUE, true } // particle and sound
             )
     };
 
@@ -71,22 +76,28 @@ public class AxtonsEmotes extends JavaPlugin {
         // Register command executors
         for (CommandItem command : commands) {
             String mainCommand = command.getCommandData()[0];
-            getCommand(mainCommand).setExecutor(new CustomCommandExecutor(command));
-            registeredCommands++;
+            PluginCommand pluginCommand = getCommand(mainCommand);
+
+            if (pluginCommand != null) {
+                pluginCommand.setExecutor(new CustomCommandExecutor(command));
+                registeredCommands++;
+            }
 
             // Register aliases
             String[] aliases = command.getAliasData();
             if (aliases != null && aliases.length > 0) {
                 for (String alias : aliases) {
-                    getCommand(alias).setExecutor(new CustomCommandExecutor(command));
-                    registeredCommands++;
+                    PluginCommand aliasCommand = getCommand(alias);
+                    if (aliasCommand != null) {
+                        aliasCommand.setExecutor(new CustomCommandExecutor(command));
+                        registeredCommands++;
+                    }
                 }
             }
         }
 
         getLogger().info("Successfully registered " + registeredCommands + " commands!");
     }
-
 
     private static class CommandItem {
 
@@ -129,81 +140,83 @@ public class AxtonsEmotes extends JavaPlugin {
 
         @Override
         public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-
-            // player vars
-            Player p = (Player) sender;
-            Player t = Bukkit.getServer().getPlayer(args[0]);
-            // command data vars
-            String cmdName = commandItem.getCommandData()[0];
-            String cmdDesc = commandItem.getCommandData()[1];
-            String cmdVerbTense = (String) commandItem.getCommandDetails()[0];
-            String cmdTheme = (String) commandItem.getCommandDetails()[1];
-            Particle execParticle = (Particle) commandItem.getExecutionConfig()[0];
-            Sound execSound = (Sound) commandItem.getExecutionConfig()[1];
-
-            if (!(sender instanceof Player)) {
-                // sender is not a player
-                sender.sendMessage("§cOnly players can execute this command!");
-                return true;
-            }
-            if (args.length == 0) {
-                // missing player argument
-                p.sendMessage("§c" + cmdDesc + ". Usage: /" + cmdName + " <player>");
-                return true;
-            }
-            if (t == null) {
-                // invalid player argument
-                p.sendMessage("§cThat player is not online!");
-                return true;
-            }
             try {
-                if (args.length == 1) {
-                    if (p.hasPermission("axtonsemotes." + cmdName)) {
-                        Location pl = p.getLocation();
-                        Location tl = t.getLocation();
 
-                        if (p == t) {
-                            if ((Boolean) commandItem.getCommandDetails()[2] == true) {
-                                // self-execution is enabled, and the target and executor are the same
-                                p.sendMessage("§" + cmdTheme + "You " + cmdVerbTense + " yourself!"); // to executing player
-                                p.playSound(pl, execSound, 0.8F, 1.3F);
-                                p.getLocation().getWorld().spawnParticle(execParticle, tl.add(0, 1, 0), 1);
-                            } else {
-                                // self-execution is disable!
-                                p.sendMessage("§cYou cannot execute that command on yourself!");
-                                return true;
-                            }
-                        } else {
-                            p.sendMessage("§" + cmdTheme + "You " + cmdVerbTense + " §l"
-                                    + t.getName() + "§r§" + cmdTheme + "!"); // to executing player
+                // command data variables
+                String cmdName = commandItem.getCommandData()[0];
+                String cmdDesc = commandItem.getCommandData()[1];
+                String cmdVerbTense = (String) commandItem.getCommandDetails()[0];
+                String cmdTheme = (String) commandItem.getCommandDetails()[1];
+                Particle execParticle = (Particle) commandItem.getExecutionConfig()[0];
+                Sound execSound = (Sound) commandItem.getExecutionConfig()[2];
+
+                if (!(sender instanceof Player)) {
+                    // sender is not a player
+                    sender.sendMessage("§cOnly players can execute this command!");
+                    return true;
+                }
+                if (args.length == 0) {
+                    // missing player argument
+                    sender.sendMessage("§cUsage: /" + cmdName + " <player>");
+                    return true;
+                }
+
+                // player variables
+                Player p = (Player) sender;
+                Player t = Bukkit.getServer().getPlayer(args[0]);
+
+                if (t == null) {
+                    // invalid player argument
+                    sender.sendMessage("§cThat player is not online or does not exist!");
+                    return true;
+                }
+
+                if (p.hasPermission("axtonsemotes." + cmdName)) {
+                    Location pl = p.getLocation();
+                    Location tl = t.getLocation();
+                    if (p == t) {
+                        if ((Boolean) commandItem.getCommandDetails()[2]) {
+                            // self-execution is enabled, and the target and executor are the same
+                            p.sendMessage(
+                                    "§" + cmdTheme + "You " + cmdVerbTense + " §lyourself§r§" + cmdTheme + "!"); // to
+                                                                                                                 // executing
+                            // player
                             p.playSound(pl, execSound, 0.8F, 1.3F);
+                            p.getLocation().getWorld().spawnParticle(execParticle, tl.add(0, 1, 0), 1);
+                        } else {
+                            // self-execution is disabled!
+                            p.sendMessage("§cYou cannot execute that command on yourself!");
+                            return true;
+                        }
+                    } else {
+                        p.sendMessage("§" + cmdTheme + "You " + cmdVerbTense + " §l"
+                                + t.getName() + "§r§" + cmdTheme + "!"); // to executing player
+                        if ((Boolean) commandItem.getExecutionConfig()[1])
+                            p.playSound(pl, execSound, 1F, 1.3F);
+                        if ((Boolean) commandItem.getExecutionConfig()[3])
                             p.getLocation().getWorld().spawnParticle(execParticle, tl.add(0, 1, 0),
                                     1);
 
-                            t.sendMessage("§" + cmdTheme + "§l" + p.getName() + "§r §" + cmdTheme
-                                    + cmdVerbTense + " you!"); // send to target player
-                            t.sendActionBar("§" + cmdTheme + cmdVerbTense.substring(0, 1).toUpperCase()
-                                    + cmdVerbTense.substring(1) + " by " + p.getName() + "!");
-                            t.playSound(tl, execSound, 0.8F, 1.3F);
+                        t.sendMessage("§" + cmdTheme + "§l" + p.getName() + "§r §" + cmdTheme
+                                + cmdVerbTense + " you!"); // send to target player
+                        t.sendActionBar("§" + cmdTheme + cmdVerbTense.substring(0, 1).toUpperCase()
+                                + cmdVerbTense.substring(1) + " by §l" + p.getName());
+                        if ((Boolean) commandItem.getExecutionConfig()[1])
+                            t.playSound(tl, execSound, 1F, 1.3F);
+                        if ((Boolean) commandItem.getExecutionConfig()[3])
                             t.getLocation().getWorld().spawnParticle(execParticle, tl.add(0, 1, 0),
                                     1);
-                        }
-
-                    } else {
-                        p.sendMessage("§cYou don't have permission to use this command.");
                     }
-                    return true;
+                } else {
+                    p.sendMessage("§cYou don't have permission to use this command.");
                 }
-            } catch (
-
-            ArrayIndexOutOfBoundsException e) {
+                return true;
+            } catch (ArrayIndexOutOfBoundsException e) {
                 // Handle the exception (e.g., log or notify the server owner)
                 sender.sendMessage(
                         "§c[Axton's Emotes] An error occurred while processing the command.");
             }
-
             return true;
-
         }
     }
 }

46ec91d593a3814d3a131b48ae661ab98a35bf02
Axton <axton@arizon.dev>
2024-07-12 04:17:56 +0100

    1.21 release

--- README.md
+++ README.md
@@ -1,21 +1,18 @@
 # Axton's Emotes Bukkit
-> An extremely lightweight social chat plugin for Bukkit/Spigot 1.20.4
+> An extremely lightweight social chat plugin for Bukkit/Spigot 1.21
 <img width="300" src="https://github.com/axtonprice/axtons-emotes/assets/37771600/cb218cd7-80af-4241-ba33-e18800b4df1d">
 <img width="300" src="https://github.com/axtonprice/axtons-emotes/assets/37771600/5884fbf1-54bd-4093-808a-e7fb910650c3">
 
 ### Commands
-- `/hug <playername>` - Hug another player
-- `/kiss <playername>` - Kiss another player
-- `/poke <playername>` - Poke another player
-- `/highfive <playername>` - High-five another player
-- `/slap <playername>` - Slap another player
-- `/thank <playername>` - Thank another player
-- `/lick <playername>` - Lick another player (??)
-- `/egg <playername>` - Throw an egg at a player
-- `/applaud <playername>` - Applaud another player
+- `/hug <player>` - Hug another player
+- `/kiss <player>` - Kiss another player
+- `/poke <player>` - Poke another player
+- `/highfive <player>` - High-five another player
+- `/slap <player>` - Slap another player
+- `/thank <player>` - Thank another player
 
 ### Configuration
 There are no configuration values available at the time of this release.
 
 ### Disclaimer
-This project was created for educational purposes and probably shouldn't be used in large-scale servers.
+This project was created for educational purposes.
---
+++ src/main/java/com/arizonsoftware/commands/Poke.java
@@ -0,0 +1,65 @@
+package com.arizonsoftware.commands;
+
+import org.bukkit.Particle;
+import org.bukkit.Sound;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+import static org.bukkit.Bukkit.getServer;
+
+public class Poke implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+
+        // Check if sender is not a player
+        if (!(sender instanceof Player)){
+            sender.sendMessage("§cOnly players can execute this command!");
+        }
+
+        // Check if targeted player is not online
+        if (getServer().getPlayer(args[0]) == null) {
+            sender.sendMessage("§cPlayer not found!");
+            return true;
+        }
+
+        // Check if sender targeted themselves
+        if (args[0].equals(sender.getName())) {
+            sender.sendMessage("§cYou can't execute this command on yourself!");
+            return true;
+        }
+
+        // Get command sender player
+        assert sender instanceof Player;
+        final Player player = (Player) sender;
+
+        // Get command argument player
+        final Player target = getServer().getPlayer(args[0]);
+
+        // Set command details
+        String colour = "e";
+        String pastTense = "poked";
+
+        // Notify sender
+        assert target != null;
+        player.sendMessage("§" + colour + "You " + pastTense + " §l" + target.getName() + "§r§" + colour + "!");
+
+        // Notify argument player
+        target.sendMessage("§" + colour + "§l" + player.getName() + "§r §" + colour + pastTense + " you!");
+
+        // Emit particles on both players
+        player.getWorld().spawnParticle(Particle.CRIT, player.getLocation(), 3, 0.5, 0.5, 0.5, 0);
+        target.getWorld().spawnParticle(Particle.CRIT, target.getLocation(), 3, 0.5, 0.5, 0.5, 0);
+
+        // Play sound to both players
+        player.getWorld().playSound(player.getLocation(), Sound.ENTITY_CHICKEN_EGG, 1, 1);
+        target.getWorld().playSound(target.getLocation(), Sound.ENTITY_CHICKEN_EGG, 1, 1);
+
+        // Output command output
+        return true;
+    }
+
+}
